60ad8afc739aba8abe8f7857c932b1902645d97f
Mirco Michel <mirco.michel@scilifelab.se>
2015-01-09 11:19:41 +0100

    updated PyPi release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='ContactVis',
-    version='0.1.5',
+    version='0.1.6',
     author='M. Michel',
     author_email='guy.inkognito42@gmail.com',
     packages=['contactvis', 'contactvis.parsing', 'contactvis.test'],
     url='https://github.com/MMichel/contact-vis.git',
     license='LICENSE.txt',
     description='Contact map plotting for predicted protein residue-residue contacts.',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
     install_requires=[
         "Biopython >= 1.59",
         "matplotlib >= 1.3.1",
